7629c1dc49407eaa4be24858c177696b26ed1a47
Filip Cieleban <29086791@students.lincoln.ac.uk>
2025-04-30 22:44:38 +0100

    Create Unique.cs

---
+++ Code/Items/Unique.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonExplorer
+{
+    internal class Unique
+    {
+        public class Rock
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public int Weight { get; set; }
+            public int Value { get; set; }
+            public Rock()
+            {
+                Name = "Rock";
+                Description = "Engravings offer no tactical advantage whatsoever.";
+                Weight = 1;
+                Value = 0;
+            }
+        }
+
+    }
+}
